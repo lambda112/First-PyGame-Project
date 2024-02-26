55314f9b40657748e0155e573f24e393e1bbbf36
Kamran Azim <Kamazim121212@gmail.com>
2024-02-26 03:28:02 +0000

    Created players with movement

---
+++ fighter.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Fighter():
+    def __init__(self, x, y) -> None:
+        self.rect = pygame.Rect((x,y, 80, 100))
+
+    def move(self):
+        SPEED = 10
+        dx = 0
+        dy = 0
+
+        # Get Keypresses
+        key = pygame.key.get_pressed()
+
+        # Movement
+        if key[pygame.K_a]:
+            dx = -SPEED
+        if key[pygame.K_d]:
+            dx = SPEED 
+
+        # Update Player Position
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255,0,0), self.rect)
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pygame
+from fighter import Fighter
 
 pygame.init()
 
 # Create Game Window
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Fighter")
-
 
 # Load background image
 bg_image = pygame.image.load("assets\\images\\background\\street_fighter_fire.jpg").convert_alpha()
@@ -18,12 +17,26 @@ def draw_bg(image, cordinates, size):
     scaled_bg = pygame.transform.scale(image, size)
     screen.blit(scaled_bg, cordinates)
 
+
+# Create Two Instances of fighters
+fighter_1 = Fighter(200, 450)
+fighter_2 = Fighter(700, 450)
+
+
 # Game Loop
 run = True
 while run:
     
     # Draw background
     draw_bg(image=bg_image, cordinates=(0,0), size =(SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Move Fighters
+    fighter_1.move()
+    fighter_2.move()
+
+    # Draw Fighters
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
 
     # Event Handler
     for event in pygame.event.get():
